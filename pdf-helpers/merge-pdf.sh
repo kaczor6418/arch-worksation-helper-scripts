5a1b7b5d4e2b08b3021988739fc820fd3747a4eb
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2023-06-29 13:22:43 +0200

    feat(pdf-helpers): add script for merging pdf files

---
+++ pdf-helpers/merge-pdf.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# merge-pdf [output.pdf] [first.pdf] [second.pdf]
+#
+# * $1 → output.pdf file name
+# * $2... → pdf files to merge
+#
+# Description:
+# Takes many pdf files and merge into one pdf.
+# Files will be merged in the same order as given pdf's
+#
+# Example: 
+# * merge-pdf merged.pdf first.pdf second.pdf
+
+gs -q -sPAPERSIZE=letter -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile="$1" ${@:1}
